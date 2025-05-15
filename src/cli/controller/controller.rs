e0bd4b51ef8262855b7337f25a7df1ab58c2a77b
Bilal Khan <bilalkhanrecovered@gmail.com>
2025-05-16 04:11:15 +0500

    added the dry run and confirm command for more functionalities in the app

--- src/cli/controller/controller.rs
+++ src/cli/controller/controller.rs
@@ -1,15 +1,30 @@
 use crate::{
     cli::command::command::Far,
-    usecases::{find::find_text, replace::replace_text},
+    usecases::{dry_run::dry_run_text, find::find_text, replace::replace_text},
 };
 use clap::Parser;
 
 pub fn control_args() {
     let args: Far = Far::parse();
-    let status = find_text(args.find.clone(), args.target.clone());
-    if status {
-        replace_text(args.target, args.find, args.replace);
+    
+    if args.dry_run {
+        if let Some(path) = args.target.clone() {
+            dry_run_text(path, args.find, args.replace);
+        } else {
+            eprintln!("Dry run needs a --target file path to preview.");
+        }
+    } else if let Some(path) = args.target.clone() {
+        let text_found = find_text(args.find.clone(), path.clone());
+        if text_found {
+            if args.confirm {
+                replace_text(path, args.find, args.replace);
+            } else {
+                eprintln!("Use --confirm to write the replace the text")
+            }
+        } else {
+            eprintln!("Err: '{}' not found in the given file", args.find);
+        }
     } else {
-        eprintln!("Err: '{}' text is not found in the given target path", args.find);
+        eprintln!("Nothing to do. Use either --target to write or --dry-run to preview.");
     }
 }
