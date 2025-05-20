a292dd013fdc31c0fc1938c78e986d6c6cfb1ae4
Bilal Khan <bilalkhanrecovered@gmail.com>
2025-05-21 04:26:30 +0500

    added another command of taking a backup before replacing the text

--- src/cli/controller/controller.rs
+++ src/cli/controller/controller.rs
@@ -2,6 +2,7 @@ use crate::{
     cli::command::command::Far,
     usecases::{dry_run::dry_run_text, find::find_text, regex::find_regex, replace::replace_text},
 };
+use std::{fs, fs::File};
 
 impl Far {
     pub fn control_args(self) {
@@ -10,7 +11,7 @@ impl Far {
         } else if let Some(path) = &self.target {
             self.find_the_text(path);
         } else {
-            eprintln!("Err: use either --target to write or --dry-run to preview.");
+            eprintln!("Err: use either --target to write or --dry-run to preview");
         }
     }
 
@@ -20,7 +21,6 @@ impl Far {
                 dry_run_text(path, find, &self.replace);
             } else if let Some(regex) = &self.regex {
                 if let Some(regex_found) = find_regex(&regex, &path) {
-                    // self.handle_replacing(path, &regex_found);
                     dry_run_text(path, &regex_found, &self.replace);
                 } else {
                     eprintln!("Err: '{}' is not found in the given file", regex);
@@ -29,15 +29,20 @@ impl Far {
                 eprintln!("Err: gmail value is not provided to find the text");
             }
         } else {
-            eprintln!("Err: dry run needs a --target file path to preview.");
+            eprintln!("Err: dry run needs a --target file path to preview");
         }
     }
 
     fn handle_replacing(&self, path: &String, find_text: &String) {
-        if self.confirm {
-            replace_text(path, find_text, &self.replace);
+        if self.backup.len() > 0 {
+            if self.confirm {
+                self.create_file();
+                replace_text(path, find_text, &self.replace);
+            } else {
+                eprintln!("Err: use --confirm to replace the text");
+            }
         } else {
-            eprintln!("Err: use --confirm to replace the text");
+            eprintln!("Err: please take the backup first before replacing the data");
         }
     }
 
@@ -58,5 +63,17 @@ impl Far {
         } else {
             eprintln!("Err: value is not provided to find the text");
         }
+    }
+
+    fn create_file(&self) {
+        let home_dir = dirs::home_dir().expect("Err: failed to get the home directory");
+        let joined_dir = home_dir.join("far");
+
+        if !joined_dir.exists() {
+            fs::create_dir(&joined_dir).expect("Err: failed to create the home directory");
+        }
+
+        let merge_path = joined_dir.join(&self.backup);
+        File::create_new(merge_path).expect("Err: failed to create the file");
     }
 }
