96b1ee640afd99edcca35fc708bbd153e5e4e5ac
Bilal Khan <bilalkhanrecovered@gamil.com>
2025-05-21 14:19:47 +0500

    separated the code properly and added some and none values to prevent the unknown errors

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ install:
 	cargo add clap --features derive
 	cargo add walkdir
 	cargo add regex
+	cargo add dirs
 
 build:
 	cargo build
--- src/cli/controller/controller.rs
+++ src/cli/controller/controller.rs
@@ -6,16 +6,45 @@ use std::{fs, fs::File};
 
 impl Far {
     pub fn control_args(self) {
-        if self.dry_run {
-            self.handle_dry_run();
-        } else if let Some(path) = &self.target {
+        if let Some(path) = &self.target {
             self.find_the_text(path);
         } else {
-            eprintln!("Err: use either --target to write or --dry-run to preview");
+            eprintln!("Err: --target flag is required to replace the text");
         }
     }
 
-    fn handle_dry_run(self) {
+    fn find_the_text(&self, path: &String) {
+        if let Some(find) = &self.find {
+            let text_found = find_text(find, path);
+            if text_found {
+                self.handle_options(path, find);
+            } else {
+                eprintln!("Err: '{}' is not found in the given file", find);
+            }
+        } else if let Some(regex) = &self.regex {
+            if let Some(regex_found) = find_regex(&regex, &path) {
+                self.handle_options(path, &regex_found);
+            } else {
+                eprintln!("Err: '{}' is not found in the given file", regex);
+            }
+        } else {
+            eprintln!("Err: value is not provided to find the text");
+        }
+    }
+
+    fn handle_options(&self, path: &String, find_text: &String) {
+        if self.confirm {
+            replace_text(path, find_text, &self.replace);
+        } else if self.dry_run {
+            self.handle_dry_run();
+        } else if let Some(backup_file) = &self.backup {
+            self.handle_file_backup(backup_file);
+        } else {
+            eprintln!("Err: use either --confirm to replace or --dry-run to preview the text");
+        }
+    }
+
+    fn handle_dry_run(&self) {
         if let Some(path) = &self.target {
             if let Some(find) = &self.find {
                 dry_run_text(path, find, &self.replace);
@@ -26,54 +55,25 @@ impl Far {
                     eprintln!("Err: '{}' is not found in the given file", regex);
                 }
             } else {
-                eprintln!("Err: gmail value is not provided to find the text");
+                eprintln!("Err: text is not provided to find it");
             }
-        } else {
-            eprintln!("Err: dry run needs a --target file path to preview");
         }
     }
 
-    fn handle_replacing(&self, path: &String, find_text: &String) {
-        if self.backup.len() > 0 {
-            if self.confirm {
-                self.create_file();
-                replace_text(path, find_text, &self.replace);
-            } else {
-                eprintln!("Err: use --confirm to replace the text");
-            }
-        } else {
-            eprintln!("Err: please take the backup first before replacing the data");
-        }
-    }
-
-    fn find_the_text(&self, path: &String) {
-        if let Some(find) = &self.find {
-            let text_found = find_text(find, path);
-            if text_found {
-                self.handle_replacing(path, find);
-            } else {
-                eprintln!("Err: '{}' is not found in the given file", find);
-            }
-        } else if let Some(regex) = &self.regex {
-            if let Some(regex_found) = find_regex(&regex, &path) {
-                self.handle_replacing(path, &regex_found);
-            } else {
-                eprintln!("Err: '{}' is not found in the given file", regex);
-            }
-        } else {
-            eprintln!("Err: value is not provided to find the text");
-        }
-    }
-
-    fn create_file(&self) {
+    fn handle_file_backup(&self, backup_file: &String) {
         let home_dir = dirs::home_dir().expect("Err: failed to get the home directory");
         let joined_dir = home_dir.join("far");
+        let merge_path = joined_dir.join(backup_file);
 
         if !joined_dir.exists() {
             fs::create_dir(&joined_dir).expect("Err: failed to create the home directory");
         }
 
-        let merge_path = joined_dir.join(&self.backup);
-        File::create_new(merge_path).expect("Err: failed to create the file");
+        if !merge_path.exists() {
+            File::create_new(&merge_path).expect("Err: failed to create the file");
+            println!("Backup data is saved in the {:?} file", merge_path);
+        } else {
+            eprintln!("Err: {:?} file already exists", merge_path);
+        }
     }
 }
