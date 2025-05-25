2588afff549d952f4ff0cd133328b40f888822f2
Bilal Khan <bilalkhanrecovered@gmail.com>
2025-05-26 01:24:02 +0500

    modified the find regex functionality

--- src/usecases/regex.rs
+++ src/usecases/regex.rs
@@ -1,18 +1,14 @@
 use regex::Regex;
-use std::fs;
-use walkdir::WalkDir;
+use std::{fs::File, io::Read};
 
 pub fn find_regex(expression: &str, path: &str) -> Option<String> {
     let re = Regex::new(&expression).unwrap();
+    let mut file_path = File::open(path).expect("Err: failed to open the file");
+    let mut file_content = String::new();
 
-    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
-        if entry.path().is_file() {
-            let content = fs::read_to_string(entry.path()).unwrap_or_default();
-
-            if let Some(m) = re.find(&content) {
-                return Some(m.as_str().to_string());
-            }
-        }
+    file_path.read_to_string(&mut file_content).unwrap();
+    if let Some(m) = re.find(&file_content) {
+        return Some(m.as_str().to_string());
     }
     None
 }
